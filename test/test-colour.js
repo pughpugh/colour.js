9bd5da347406197f8ad0b9d22547f4e0afd8eef9
Hugh Gallagher <hugh@hcgallagher.co.uk>
2013-03-11 08:53:17 +0000

    Added some more conversion tests

--- test/test-colour.js
+++ test/test-colour.js
@@ -4,6 +4,58 @@ testColours = [
     { title: 'Green', RGB: { r: 82,  g: 166, b: 65  }, HSV: { h: 110, s: 61, v: 65  }, HEX: '#52A641', lightOrDark: 'light'  }
 ];
 
+// - Conversion
+
+QUnit
+    .cases( testColours )
+    .test( "RGB to HSV ", function(params){
+        var model = 'RGB'
+        var colour = new Colour(model, params[model]);
+        deepEqual( colour.RGBToHSV( params.RGB ), params.HSV, 'Conversion' );
+     });
+
+QUnit
+    .cases( testColours )
+    .test( "RGB to HEX ", function(params){
+        var model = 'RGB'
+        var colour = new Colour(model, params[model]);
+        deepEqual( colour.RGBToHEX( params.RGB ), params.HEX, 'Conversion' );
+     });
+
+QUnit
+    .cases( testColours )
+    .test( "HSV to RGB ", function(params){
+        var model = 'HSV'
+        var colour = new Colour(model, params[model]);
+        deepEqual( colour.HSVToRGB( params.HSV ), params.RGB, 'Conversion' );
+     });
+
+QUnit
+    .cases( testColours )
+    .test( "HSV to HEX ", function(params){
+        var model = 'HSV'
+        var colour = new Colour(model, params[model]);
+        deepEqual( colour.HSVToHEX( params.HSV ), params.HEX, 'Conversion' );
+     });
+
+QUnit
+    .cases( testColours )
+    .test( "HEX to RGB ", function(params){
+        var model = 'HEX'
+        var colour = new Colour(model, params[model]);
+        deepEqual( colour.HEXToRGB( params.HEX ), params.RGB, 'Conversion' );
+     });
+
+QUnit
+    .cases( testColours )
+    .test( "HEX to HSV ", function(params){
+        var model = 'HEX'
+        var colour = new Colour(model, params[model]);
+        deepEqual( colour.HEXToHSV( params.HEX ), params.HSV, 'Conversion' );
+     });
+
+// - Init & Getters
+
 QUnit
     .cases( testColours )
     .test( "New RGB ", function(params){
